432fd326bcd84641543afeb85decee8b6d71520a
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-13 00:13:11 -0300

    chore: add header style

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.header`
+  height: 70px;
+  width: 100%;
+  padding: 0 80px;
+  display: flex;
+  align-items: center;
+  margin-bottom: 80px;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+  font-size: 28px;
+  color: #ffffff;
+
+  .bold {
+    font-weight: 900;
+  }
+`
