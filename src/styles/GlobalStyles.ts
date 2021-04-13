4eacd3a2851398729ad6db0c5c028e28c6239ed4
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-12 23:53:59 -0300

    chore: add global style

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    color: #ffffff;
+    background-color: #52b69a;
+  }
+
+`
