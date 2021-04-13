db0417e9294ada58189855fbcc84ed0895850df9
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-12 23:54:28 -0300

    chore: add raised button style

---
+++ src/components/RaisedButton/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.button`
+  height: 56px;
+  padding: 0 28px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  user-select: none;
+  background-color: #fff;
+  color: #52b69a;
+  font-weight: 600;
+  font-size: 20px;
+  border: none;
+  border-radius: 12px;
+  outline: none;
+`
