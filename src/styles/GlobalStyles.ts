bd3a9add08684bde5a7055be325db4c2022da1b4
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-12 23:56:21 -0300

    chore: change color background

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -10,7 +10,7 @@ export const GlobalStyle = createGlobalStyle`
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     color: #ffffff;
-    background-color: #52b69a;
+    background-color: #0466c8;
   }
 
 `
