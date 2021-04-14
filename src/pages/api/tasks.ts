7d9d78b92e985ecb85cc43650b64ee4c23f580c6
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-14 01:37:26 -0300

    chore: change data

--- src/pages/api/tasks.ts
+++ src/pages/api/tasks.ts
@@ -3,7 +3,18 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ): void {
-  res
-    .status(200)
-    .json([{ title: 'Lorem Ipsum', finished: false, createdAt: new Date() }])
+  res.status(200).json([
+    {
+      title: 'Define project technologies and architecture',
+      finished: true,
+      createdAt: new Date(),
+      id: 1,
+    },
+    {
+      title: 'Refactor login screen to make it responsive',
+      finished: false,
+      createdAt: new Date(),
+      id: 2,
+    },
+  ])
 }
