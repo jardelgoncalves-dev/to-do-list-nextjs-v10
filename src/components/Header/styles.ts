5630e024890441fb6a84c7691f31e00dddc2db4f
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-13 23:19:25 -0300

    refactor: change padding

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,12 +1,12 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.header`
+  background-color: var(--primary-color);
   height: 70px;
   width: 100%;
   padding: 0 80px;
   display: flex;
   align-items: center;
-  margin-bottom: 80px;
   border-bottom: 1px solid rgba(255, 255, 255, 0.1);
   font-size: 28px;
   color: #ffffff;
