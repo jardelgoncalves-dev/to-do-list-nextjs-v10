c0d527161c4ddcb3a28753460cf55f79ed530bfd
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-15 01:31:11 -0300

    chore: testing components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,55 @@
+import { AxiosError } from 'axios'
+import { finished } from 'node:stream'
+import { useState } from 'react'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+import { ListTask } from '../components/ListTask'
+
+import { AddTaskContainer } from '../conteiners/AddTask'
+import api from '../services/api'
+
 export default function Home() {
-  return <h1>Hello</h1>
+  const queryClient = useQueryClient()
+
+  const [title, setTitle] = useState('')
+  const { isLoading, isError, data } = useQuery('tasks', () =>
+    api.get('/tasks').then((response) => response.data)
+  )
+
+  const createMutation = useMutation<Response, AxiosError, string>(
+    () => api.post('/tasks', { title }),
+    {
+      onSuccess: () => queryClient.invalidateQueries('tasks'),
+    }
+  )
+
+  const finishedtaskMutation = useMutation<
+    Response,
+    AxiosError,
+    number | undefined
+  >((id) => api.put(`/tasks/${id}`), {
+    onSuccess: () => queryClient.invalidateQueries('tasks'),
+  })
+
+  return (
+    <>
+      <AddTaskContainer
+        onPress={() =>
+          createMutation.mutate(title, {
+            onSuccess: () => {
+              setTitle('')
+            },
+          })
+        }
+        onChange={(value: string) => setTitle(value)}
+        inputPlaceholder="inform your task to do"
+        inputValue={title}
+      />
+      <ListTask
+        tasks={data}
+        onCheck={(id) => {
+          finishedtaskMutation.mutate(id, {})
+        }}
+      />
+    </>
+  )
 }
