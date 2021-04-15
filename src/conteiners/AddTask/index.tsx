e43a638e4f2aa3314cdc16a0124db57fb63bad3a
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-15 01:30:31 -0300

    feat: add AddTask container

---
+++ src/conteiners/AddTask/index.tsx
@@ -0,0 +1,28 @@
+import { RaisedButton } from '../../components'
+import { Input } from '../../components/Input'
+import { Wrapper } from './styles'
+
+interface AddTaskContainerProp {
+  onPress: () => void
+  onChange: (value: string) => void
+  inputValue?: string
+  inputPlaceholder?: string
+}
+
+export function AddTaskContainer({
+  onPress,
+  onChange,
+  inputValue,
+  inputPlaceholder,
+}: AddTaskContainerProp) {
+  return (
+    <Wrapper>
+      <Input
+        onChange={onChange}
+        placeholder={inputPlaceholder}
+        value={inputValue}
+      />
+      <RaisedButton text="Add Task" onPress={onPress} />
+    </Wrapper>
+  )
+}
