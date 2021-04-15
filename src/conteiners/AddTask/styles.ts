3ef2f3cf3b1a5f6747b03673faa41d75d704200d
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-15 01:30:55 -0300

    chore: add style to add task

---
+++ src/conteiners/AddTask/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 60px 20px 120px 20px;
+  background-color: var(--primary-color);
+  align-items: center;
+  justify-content: center;
+
+  button {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+`
