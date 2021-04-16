435c305a7246e2a06851101a75edb06f7a73c710
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-16 00:58:35 -0300

    refactor: add methods of the repository

--- src/pages/api/tasks/[id].ts
+++ src/pages/api/tasks/[id].ts
@@ -1,12 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { ApiError } from '../../../helpers/api-error'
+import * as userRepository from '../../../helpers/task-repository'
 
-import { update } from '.'
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-): void {
-  if (req.method === 'PUT') return update(req, res)
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'PUT') return updateTask(req, res)
+  if (req.method === 'DELETE') return destroyTask(req, res)
 
   return res.status(405).json({ message: 'Method not allowed' })
+}
+
+async function destroyTask(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { id } = req.query
+    const data = await userRepository.destroy(Number(id))
+
+    res.status(204).json(data)
+  } catch (error) {
+    return ApiError.errorHandler(error, res)
+  }
+}
+
+async function updateTask(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { id } = req.query
+    const task = req.body
+
+    const data = await userRepository.update(Number(id), task)
+
+    res.status(200).json(data)
+  } catch (error) {
+    return ApiError.errorHandler(error, res)
+  }
 }
