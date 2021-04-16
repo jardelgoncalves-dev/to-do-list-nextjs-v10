534c35402a2df876dd39a618f051b31dfc8085b3
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-16 00:54:32 -0300

    chore: add next config

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',
+      }
+    }
+
+    return config
+  },
+}
