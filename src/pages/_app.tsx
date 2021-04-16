08606f07231504dac7fc5d85c932f5b4756e05bf
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-16 00:59:00 -0300

    chore: add title, font and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import Head from 'next/head'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Container } from '../components/Container'
 import { Header } from '../components/Header'
@@ -12,13 +13,24 @@ type Props = {
 function MyApp({ Component, pageProps }: Props) {
   const queryClient = new QueryClient()
   return (
-    <QueryClientProvider client={queryClient}>
-      <GlobalStyle />
-      <Header />
-      <Container>
-        <Component {...pageProps} />
-      </Container>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="shortcut icon" href="/static/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap"
+          rel="stylesheet"
+        />
+        <title>to.do</title>
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <Header />
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </QueryClientProvider>
+    </>
   )
 }
 
