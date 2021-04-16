41a287da900fac2fbe2f839dac39293880d30680
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-16 00:55:41 -0300

    feat: add close icon

---
+++ src/components/Icon/Close.tsx
@@ -0,0 +1,19 @@
+export function Close() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="25"
+      height="25"
+      fill="none"
+      viewBox="0 0 25 25"
+    >
+      <path
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="3"
+        d="M18.795 6.551l-12 12M6.795 6.551l12 12"
+      ></path>
+    </svg>
+  )
+}
--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,14 +1,17 @@
 import { Edit } from './Edit'
 import { Trash } from './Trash'
+import { Close } from './Close'
 
 const icons = {
   edit: <Edit />,
   trash: <Trash />,
+  close: <Close />,
 }
 
 export enum IconName {
   edit = 'edit',
   trash = 'trash',
+  close = 'close',
 }
 
 interface IconProp {
