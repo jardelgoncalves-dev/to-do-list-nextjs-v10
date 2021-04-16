31dfcb7cc270286bdc5c043ef5642504c1e0ed40
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-16 00:59:15 -0300

    chore: remove link to head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -31,13 +31,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
